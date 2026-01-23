88e66964071273d19bebb4e17960a4deecd526ad
MuhammedJaseel <jaseelmanamulli@gmail.com>
2026-01-23 12:24:29 +0400

    OP

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Name: Ticket API Service \nVersion: 0.0.13\nStatus: Running';
+    return 'Name: Ticket API Service \nVersion: 0.0.14\nStatus: Running';
   }
 }
